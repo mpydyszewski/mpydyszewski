48d935ee26c6ddf8a1ef77eb6cc622433e934332
Michał Pydyszewski <mpydyszewski@p29-9.labpk.inf.ug.edu.pl>
2016-11-05 10:44:31 +0100

    cokolwiek

---
+++ test2.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main ()  {
+  int x;
+  scanf("%d",&x);
+  printf("%d\n",x);
+      if (x%2 == 0 && x%3 == 0 && x%5 == 0) {
+        printf("Dzieli sie przez 2,3 i 5\n");
+    } else if (x%2==0 && x%3==0) {
+        printf("Dzieli sie przez 2 i 3\n");
+    } else if ((x%2==0) && (x%5==0)) {
+        printf("Dzieli sie przez 2 i 5\n");
+    } else if ((x%3==0) && (x%5==0)) {
+        printf("Dzieli sie przez 3 i 5\n");
+    } else if ((x%2==0)) {
+        printf("Dzieli sie przez 2\n");
+    } else if ((x%3==0)) {
+        printf("Dzieli sie przez 3\n");
+    } else if (x%5==0) {
+        printf("Dzieli sie przez 5\n");
+
+    }
+    return 0;
+  }
